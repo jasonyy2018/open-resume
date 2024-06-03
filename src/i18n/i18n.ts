142dc5b0d3ce583c1c8b58143855e38bc19f0e54
jason <290899973@qq.com>
2024-06-03 13:33:15 +0800

    Update i18n.ts

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,27 +1,41 @@
-// src/i18n/i18n.ts
-import i18n from 'i18next';
+import i18n, { InitOptions } from 'i18next';
+import HttpApi from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpApi from 'i18next-http-backend';
+
+interface ConfigOptions extends InitOptions {
+  backend: {
+    loadPath: string;
+  };
+  detection: {
+    order: string[];
+    caches: string[];
+  };
+  react: {
+    useSuspense: boolean;
+  };
+}
+
+// 确保配置的类型正确
+const options: ConfigOptions = {
+  fallbackLng: 'en',
+  debug: true,
+  backend: {
+    loadPath: '/locales/{{lng}}/{{ns}}.json',
+  },
+  detection: {
+    order: ['localStorage', 'cookie', 'htmlTag', 'path', 'subdomain'],
+    caches: ['localStorage', 'cookie'],
+  },
+  react: {
+    useSuspense: false,
+  },
+};
 
 i18n
   .use(HttpApi)
   .use(LanguageDetector)
   .use(initReactI18next)
-  .init({
-    fallbackLng: 'en',
-    debug: true,
-    backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
-    },
-    detection: {
-      // 配置语言检测器
-      order: ['querystring', 'cookie', 'localStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
-      caches: ['cookie']
-    },
-    react: {
-      useSuspense: false,
-    }
-  });
+  .init(options);
 
 export default i18n;
