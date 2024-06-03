b9315286f7de8a4e1d6f302825d518d9f31f1395
jason <290899973@qq.com>
2024-06-03 12:40:28 +0800

    Create i18n.ts

---
+++ src/i18n/i18n.ts
@@ -0,0 +1,19 @@
+// src/i18n/i18n.ts
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+
+i18n
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
+  });
+
+export default i18n;
